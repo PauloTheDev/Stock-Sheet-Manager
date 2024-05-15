6b049c1a145edf4e94befd615f2efb97af9566a0
PauloTheDev <pauloprogramacaojs@gmail.com>
2024-05-15 15:54:26 -0300

    The register is now work

--- main.py
+++ main.py
@@ -23,55 +23,88 @@ def insertDateToBrazilianFormart(): ## date format
     return currentlyDate
     
 def insertDataOnSheets(positionsValuesInOrder): # This function will check if the currently position is empty or not. If is empty, the empty space will be fiiled by the datas from inputs. Observartion: This functions just saves on Register Sheet.
-    position = 3
-
-    strPosition = str(position)
-
-    cellColumnPosition = ['A','B','C','D','E']
-
-    cellRegisterSelect = [registerSheet['A'+strPosition].value,registerSheet['B'+strPosition].value,registerSheet['C'+strPosition].value,registerSheet['D'+strPosition].value,registerSheet['E'+strPosition].value]
-
-    for i in range(0,5): # this for loop, will check if all positions of sheet is empty
-       # while cellRegisterSelect[i] != None:
-        if cellColumnPosition[i] != None:
-            print('bla')
-        else:
-            cellRegisterSelect[i] = str(positionsValuesInOrder[i])
-        
-       
-    cellRegisterSelect[i] = positionsValuesInOrder[i]
-    print(cellColumnPosition)
-    print(cellRegisterSelect)
+   
+   pos = 3
+   
+   i = 0
+   columnsPosition = ['A','B','C','D','E']
+   
+   while i < 5:
+    posStr = str(pos)
+    positions = str(columnsPosition[i]+posStr)
     
+    if registerSheet[positions].value == None:
+           registerSheet[positions].value = positionsValuesInOrder[i]
+           i = i + 1
+           
+    else:
+           
+           pos = pos + 1
+    #explaing the function: the while will verify if the i (index) is lower than 5. While i is lower than 5 the loop will verify if the currently position in sheet is free. 
 
-      
-
+def MakeChangesOnStock (device, qtd, inOrOut):
+    tecladoValueCell = stockSheet['B3'].value
+    
 # ====== THE MAIN FUNCIONALITY ====== #
 
 print('Bem-vindo ao sistema de gerenciamento de equipamentos da TI \n' + pyfiglet.figlet_format('Voxline', font='slant'))
 
-device = str.upper(input('Qual equipamento vai ser movimentado: '))
-qtd = int(input('Quantos {}s serão movimentados? '.format(device)))   
-statusCondicional = 0
-while statusCondicional == 0:
+statusCondicionalDevice = False
+while statusCondicionalDevice == False:
+    device = str.upper(input('Qual equipamento vai ser movimentado: '))
+
+    if device == 'TECLADO' or device == 'MOUSE' or device == 'HEADSET KJ' or device == 'HEADSET PNP':
+        statusCondicionalDevice = True
+
+    elif device == 'PNP' or device == 'KJ':
+        device = 'HEADSET'+' '+ device
+        statusCondicionalDevice=True
+
+    elif device == 'HEADSET': #this condicional will be used when the user write just "headset"
+        chooseTypeOfHeadset = str.upper(input('Você digitou {}. Seria KJ ou PnP? '.format(device)))
+
+        statusCondicionalHeadset = False
+        while statusCondicionalHeadset == False:
+            if chooseTypeOfHeadset == 'PNP' or chooseTypeOfHeadset == 'KJ':
+                device = device + ' ' + chooseTypeOfHeadset.capitalize()
+                statusCondicionalHeadset = True
+                statusCondicionalDevice = True
+   
+    else:
+        print('Você digitou {} e isso não foi reconhecido pelo gerenciador. Digite novamente.'.format(device))
+    
+    qtd = int(input('Qual a quantidade? '))
+    
+
+
+
+statusCondicionalTicket = False
+while statusCondicionalTicket == False:
      ticketCondicional = str.upper(input('Tem número de chamado?  '))
     
      if ticketCondicional == 'SIM' or ticketCondicional == '':
         ticket = input('Insira o número do chamado: ')
-        statusCondicional = statusCondicional + 1
+        statusCondicionalTicket = True
      elif ticketCondicional == 'NÃO' or ticketCondicional == 'NAO' or ticketCondicional == 'N':
         print('O campo CHAMADO ficará NULO')
         ticket = 'NULO'
-        statusCondicional = statusCondicional + 1
+        statusCondicionalTicket = True
      else: 
         print('Resposta não reconhecida. Responda com SIM ou NÃO')
     
-addOrChange = str.upper(input('O {} será uma troca ou adição à PA? '.format(device)))
+statusCondicionalAddOrChange = False
+while statusCondicionalAddOrChange == False:
+    addOrChange = str.upper(input('O {} será uma entrada ou saída de equipamento? '.format(device)))
+    if addOrChange == 'ENTRADA' or addOrChange == 'SAÍDA' or addOrChange == 'SAIDA':
+        statusCondicionalAddOrChange = True
+    else:
+        print('Por favor, escreva ENTRADA ou SAIDA')
 
 currentDate = insertDateToBrazilianFormart()
 
 positionsValuesInOrder = [ticket,device,qtd, addOrChange, currentDate ]
 
+insertDataOnSheets(positionsValuesInOrder)
+wb.save('Estoque e registro.xlsx')
+print('Registro salvo com sucesso.')
 
-
-wb.save('Estoque e registro.xlsx') 
